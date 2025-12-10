d9c48adfb936cab3736f968b18866dd9c602c358
WindForest <munkored@yandex.ru>
2025-12-10 14:54:53 +0700

    created iot_device.hrl

---
+++ src/iot_device.hrl
@@ -0,0 +1,13 @@
+
+-type id() :: 1..9_999_999_9999.
+-type metrics() :: { atom(), integer()}.
+
+-record(iot_device, {
+    id              :: id(),
+    name            :: string(),
+    address         :: string(),
+    temperature     :: float(),
+    metrics = []    :: [metrics()]
+}).
+
+
