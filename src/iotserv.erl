c1501f98aa9491b65aaf038401f0cebe6ecb1a50
WindForest <munkored@yandex.ru>
2025-12-19 09:46:53 +0700

    added customer API funs and client funs

--- src/iotserv.erl
+++ src/iotserv.erl
@@ -2,14 +2,40 @@
 -module(iotserv).
 -author("MoonNew").
 
--export([]).
+-export([start_link/0, start_link/1, stop/0]).
+-export([add_device/5, delete_device_by_id/1, find_device_by_id/1, change_device_by_id/3]).
 -export([init/1, terminate/2, handle_call/3, handle_cast/2]).
 
 -behavior(gen_server).
 
 -include("iot_device.hrl").
 
-%% Callback fun
+%% Exported client functions
+
+start_link() ->
+    {ok, FileName} = application:get_env(dets_name),
+    start_link(FileName).
+
+start_link(FileName) ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, FileName, []).
+
+stop() ->
+    gen_server:cast(?MODULE, stop).
+
+%% Customer Services API
+add_device(Id, Name, Address, Temperature, Metrics) ->
+    gen_server:call(?MODULE, {add_device, Id, Name, Address, Temperature, Metrics}).
+
+delete_device_by_id(Id) ->
+    gen_server:call(?MODULE, {delete_device, Id}).
+
+find_device_by_id(Id) ->
+    gen_server:call(?MODULE, {find_device_by_id(Id)}).
+
+change_device_by_id(Id, FieldToUpdate, NewValue) ->
+    gen_server:call(?MODULE, {change_device, FieldToUpdate, NewValue}).
+
+%% Callback functions
 init(FileName) ->
     iotserv_db:create_tables(FileName),
     iotserv_db:restore_database(),
@@ -36,10 +62,9 @@ handle_call({find_device, Id}, _From, LoopData) ->
     {reply, Reply, LoopData};
 
 %% Изменение только одного параметра.
-handle_call({change_device, Id, Field, NewValue}, _From, LoopData) ->
-    Reply = iotserv_db:update_device_by_id(Id, Field, NewValue),
+handle_call({change_device, Id, FieldToUpdate, NewValue}, _From, LoopData) ->
+    Reply = iotserv_db:update_device_by_id(Id, FieldToUpdate, NewValue),
     {reply, Reply, LoopData}.
 
 handle_cast(stop, LoopData) ->
     {stop, normal, LoopData}.
-
